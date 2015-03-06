e597fc3b1b11d9962d1ce6d3964be1f6c7d0285d
Jeffrey Lee <jlee42@gmail.com>
2015-03-06 09:56:56 -0600

    Adds environment variable for devise's secret key.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,13 +4,13 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = '887d48aca3c036484eadf6b3f29308af0724c56bb833e7b1729d9948537cf91c7564a5870f07f62952dfb7357c96b91fab44c9dd50b4f22ce93c0b77031f53da'
+  config.secret_key = ENV['SECRET_KEY']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'user@domain.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
